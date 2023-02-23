6dd2c3f12a1aab8316dfee16192aa678897f414e
tobias-roy <tobiasroy94@gmail.com>
2023-02-23 20:50:27 +0100

    added all them daym cards

---
+++ src/routes/Card.svelte
@@ -0,0 +1,131 @@
+<script>
+  //Club Cards
+  import club10 from '$lib/svg/clubs/10.svg';
+  import club2 from '$lib/svg/clubs/2.svg';
+  import club3 from '$lib/svg/clubs/3.svg';
+  import club4 from '$lib/svg/clubs/4.svg';
+  import club5 from '$lib/svg/clubs/5.svg';
+  import club6 from '$lib/svg/clubs/6.svg';
+  import club7 from '$lib/svg/clubs/7.svg';
+  import club8 from '$lib/svg/clubs/8.svg';
+  import club9 from '$lib/svg/clubs/9.svg';
+  import clubAce from '$lib/svg/clubs/ace.svg';
+  import clubJack from '$lib/svg/clubs/jack.svg';
+  import clubKing from '$lib/svg/clubs/king.svg';
+  import clubQueen from '$lib/svg/clubs/queen.svg';
+//Diamond Cards
+  import diamond10 from '$lib/svg/diamonds/10.svg';
+  import diamond2 from '$lib/svg/diamonds/2.svg';
+  import diamond3 from '$lib/svg/diamonds/3.svg';
+  import diamond4 from '$lib/svg/diamonds/4.svg';
+  import diamond5 from '$lib/svg/diamonds/5.svg';
+  import diamond6 from '$lib/svg/diamonds/6.svg';
+  import diamond7 from '$lib/svg/diamonds/7.svg';
+  import diamond8 from '$lib/svg/diamonds/8.svg';
+  import diamond9 from '$lib/svg/diamonds/9.svg';
+  import diamondAce from '$lib/svg/diamonds/ace.svg';
+  import diamondJack from '$lib/svg/diamonds/jack.svg';
+  import diamondKing from '$lib/svg/diamonds/king.svg';
+  import diamondQueen from '$lib/svg/diamonds/queen.svg';
+//Heart Cards
+  import heart10 from '$lib/svg/hearts/10.svg';
+  import heart2 from '$lib/svg/hearts/2.svg';
+  import heart3 from '$lib/svg/hearts/3.svg';
+  import heart4 from '$lib/svg/hearts/4.svg';
+  import heart5 from '$lib/svg/hearts/5.svg';
+  import heart6 from '$lib/svg/hearts/6.svg';
+  import heart7 from '$lib/svg/hearts/7.svg';
+  import heart8 from '$lib/svg/hearts/8.svg';
+  import heart9 from '$lib/svg/hearts/9.svg';
+  import heartAce from '$lib/svg/hearts/ace.svg';
+  import heartJack from '$lib/svg/hearts/jack.svg';
+  import heartKing from '$lib/svg/hearts/king.svg';
+  import heartQueen from '$lib/svg/hearts/queen.svg';
+//Spade Cards
+  import spade10 from '$lib/svg/spades/10.svg';
+  import spade2 from '$lib/svg/spades/2.svg';
+  import spade3 from '$lib/svg/spades/3.svg';
+  import spade4 from '$lib/svg/spades/4.svg';
+  import spade5 from '$lib/svg/spades/5.svg';
+  import spade6 from '$lib/svg/spades/6.svg';
+  import spade7 from '$lib/svg/spades/7.svg';
+  import spade8 from '$lib/svg/spades/8.svg';
+  import spade9 from '$lib/svg/spades/9.svg';
+  import spadeAce from '$lib/svg/spades/ace.svg';
+  import spadeJack from '$lib/svg/spades/jack.svg';
+  import spadeKing from '$lib/svg/spades/king.svg';
+  import spadeQueen from '$lib/svg/spades/queen.svg';
+
+
+  const clubs = {
+    2: club2,
+    3: club3,
+    4: club4,
+    5: club5,
+    6: club6,
+    7: club7,
+    8: club8,
+    9: club9,
+    10: club10,
+    jack: clubJack,
+    queen: clubQueen,
+    king: clubKing,
+    ace: clubAce
+  };
+  
+  const diamonds = {
+    2: diamond2,
+    3: diamond3,
+    4: diamond4,
+    5: diamond5,
+    6: diamond6,
+    7: diamond7,
+    8: diamond8,
+    9: diamond9,
+    10: diamond10,
+    jack: diamondJack,
+    queen: diamondQueen,
+    king: diamondKing,
+    ace: diamondAce
+  };
+
+  const hearts = {
+    2: heart2,
+    3: heart3,
+    4: heart4,
+    5: heart5,
+    6: heart6,
+    7: heart7,
+    8: heart8,
+    9: heart9,
+    10: heart10,
+    jack: heartJack,
+    queen: heartQueen,
+    king: heartKing,
+    ace: heartAce
+  };
+
+  const spades = {
+    2: spade2,
+    3: spade3,
+    4: spade4,
+    5: spade5,
+    6: spade6,
+    7: spade7,
+    8: spade8,
+    9: spade9,
+    10: spade10,
+    jack: spadeJack,
+    queen: spadeQueen,
+    king: spadeKing,
+    ace: spadeAce
+  };
+</script>
+
+<div>
+
+</div>
+
+<style>
+
+</style>
